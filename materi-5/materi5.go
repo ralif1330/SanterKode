2196a9bc309d19b1dc3cb607d993a01875c37635
ralif1330 <ilmudunia75@gmail.com>
2023-09-05 12:57:10 +0700

    update materi function

---
+++ materi-5/materi5.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Initial awal")
+
+	printAngka(1, 2)
+}
+
+// function parameter
+
+func printAngka(angka1 int, angka2 int) {
+	fmt.Println("ini baris  ", angka1)
+	fmt.Println("ini baris ", angka2)
+}
+
+// function return value
