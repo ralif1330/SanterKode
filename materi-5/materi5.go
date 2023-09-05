7788574cf5cd53b66bdb34923abe9c3d4658407b
ralif1330 <ilmudunia75@gmail.com>
2023-09-05 13:46:33 +0700

    update materi function

--- materi-5/materi5.go
+++ materi-5/materi5.go
@@ -2,18 +2,33 @@ package main
 
 import "fmt"
 
-func main() {
-
-	fmt.Println("Initial awal")
-
-	printAngka(1, 2)
-}
-
 // function parameter
 
 func printAngka(angka1 int, angka2 int) {
-	fmt.Println("ini baris  ", angka1)
-	fmt.Println("ini baris ", angka2)
+	fmt.Println("ini baris", angka1)
+	fmt.Println("ini baris", angka2)
 }
 
 // function return value
+
+func introduction(name string) string {
+	return "Hello my name is " + name
+
+}
+
+func main() {
+
+	fmt.Println("Initial awal")
+	fmt.Println("")
+
+	// function parameter
+	printAngka(1, 2)
+	fmt.Println("")
+
+	// function return value
+	// --panggil langsung
+	fmt.Println(introduction("ralif"))
+	// --panggil menggunakan variabel
+	result := introduction("ralif")
+	fmt.Println(result)
+}
