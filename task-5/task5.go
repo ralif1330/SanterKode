e249a6391b73cb573a13a6fa7eec68a1aff03504
ralif1330 <ilmudunia75@gmail.com>
2023-09-06 21:39:36 +0700

    update

--- task-5/task5.go
+++ task-5/task5.go
@@ -2,22 +2,63 @@ package main
 
 import "fmt"
 
+// soal 1
+
+func luasPersegiPanjang(panjang int, lebar int) int {
+	return panjang * lebar
+}
+
+func kelilingPersegiPanjang(panjang int, lebar int) int {
+	return (panjang + lebar) * 2
+}
+
+func volumeBalok(panjang int, lebar int, tinggi int) int {
+	return panjang * lebar * tinggi
+}
+
+// soal 2
+
+func introduce() (nama string, sex string, job string, umur int) {
+	nama = "John"
+	sex = "laki-laki"
+	job = "penulis"
+	umur = 30
+
+	return
+}
+
+func introduce2() (nama string, sex string, job string, umur int) {
+	nama = "Sarah"
+	sex = "perempuan"
+	job = "model"
+	umur = 28
+
+	return
+}
+
 func main() {
-	fmt.Println("tes 1 bcxer ")
 
-	printHello()
+	// soal 1
 
-	fmt.Println(introduction("Reza"))
-}
+	panjang := 12
+	lebar := 4
+	tinggi := 8
 
-// function parameter pertamam
+	luas := luasPersegiPanjang(panjang, lebar)
+	keliling := kelilingPersegiPanjang(panjang, lebar)
+	volume := volumeBalok(panjang, lebar, tinggi)
 
-func printHello() {
-	fmt.Println("Hallo ini baris pertama nyoba function")
-}
+	fmt.Println("hasil luas persegi panjang : ", luas)
+	fmt.Println("hasil keliling persegi panjang : ", keliling)
+	fmt.Println("hasil volume balok : ", volume)
+	fmt.Println("")
 
-// function return value
+	// soal 2
 
-func introduction(name string) string {
-	return "Hello my name is " + name
+	john, gender, work, age := introduce()
+	fmt.Println(john, gender, work, age) // Menampilkan "Pak John adalah seorang penulis yang berusia 30 tahun"
+
+	sarah, gender, work, age := introduce2()
+	fmt.Println(sarah, gender, work, age) // Menampilkan "Bu Sarah adalah seorang model yang berusia 28 tahun"
+
 }
